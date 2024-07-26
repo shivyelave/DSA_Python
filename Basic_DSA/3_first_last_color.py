97649c04ae7b95b5197ef297bc010e467ddad79e
shivyelave <syelave20ite@student.mes.ac.in>
2024-07-26 14:24:56 +0530

    [Shiv] Add: Display first and last color of list

---
+++ Basic_DSA/3_first_last_color.py
@@ -0,0 +1,23 @@
+'''
+
+    @Author: Shivraj Yelave
+    @Date: 25-02-24
+    @Last modified by: Shivraj Yelave
+    @Last modified time:
+    @Title: Give first and last color
+
+'''
+
+def main():
+    # Define a list of colors
+    color_list = ["Red", "Green", "White", "Black"]
+    
+    # Print the first color in the list
+    print(f"First Color: {color_list[0]}")
+    
+    # Print the last color in the list
+    print(f"Last Color: {color_list[-1]}")
+
+if __name__ == '__main__':
+    # Call the main function when the script is executed
+    main()
